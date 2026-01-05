438d2ce21bd91dd54d6bad925a56763c898a5fea
SValenziano <senorvalenz@gmail.com>
2026-01-05 16:10:49 -0500

    Before starting 1.12

--- fullstackopen/01-intro-to-react/part1-1/src/App.jsx
+++ fullstackopen/01-intro-to-react/part1-1/src/App.jsx
@@ -1,45 +1,24 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const sum = good + neutral + bad;
-  const average = (good / sum).toFixed(2);
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button label={"good"} onClick={() => setGood(i => i + 1)}></Button>
-      <Button label={"neutral"} onClick={() => setNeutral(i => i + 1)}></Button>
-      <Button label={"bad"} onClick={() => setBad(i => i + 1)}></Button>
-      <h1>statistics</h1>
-      <Stats good={good} neutral={neutral} bad={bad} sum={sum} average={average}/>
+      {anecdotes[selected]}
     </div>
   )
-}
-
-function Button({ label, onClick }) {
-  return <button onClick={onClick}>{label}</button>
-}
-
-function Stats({ good, neutral, bad, sum, average }) {
-  if (sum === 0) {
-    return <p>No feedback given, yet...</p>
-  }
-  return (
-    <>
-      <StatsLine text="good" value={good}/>
-      <StatsLine text="neutral" value={neutral}/>
-      <StatsLine text="bad" value={bad}/>
-      <StatsLine text="sum" value={sum}/>
-      <StatsLine text="average" value={average + '%'}/>
-    </>
-  )
-}
-
-function StatsLine({ text, value }) {
-  return <p>{text} {value}</p>
 }
 
 export default App
