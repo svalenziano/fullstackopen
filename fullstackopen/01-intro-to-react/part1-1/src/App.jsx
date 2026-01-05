a3aef93bad271a82bd769514fcb51a65e7868bfb
SValenziano <senorvalenz@gmail.com>
2026-01-05 16:14:52 -0500

    1.12 complete

--- fullstackopen/01-intro-to-react/part1-1/src/App.jsx
+++ fullstackopen/01-intro-to-react/part1-1/src/App.jsx
@@ -14,9 +14,14 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
 
+  function setRandom() {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  }
+
   return (
     <div>
       {anecdotes[selected]}
+      <button onClick={setRandom} style={{display: "block"}}>Fetch</button>
     </div>
   )
 }
