4b66d054607e78447d134ce34a0fbd9d78d409b0
SValenziano <senorvalenz@gmail.com>
2026-01-05 15:01:20 -0500

    Two counters with history

--- fullstackopen/01-intro-to-react/part1-1/src/App.jsx
+++ fullstackopen/01-intro-to-react/part1-1/src/App.jsx
@@ -1,67 +1,51 @@
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  function increment() {
-    setCount(i => i + 1);
-  }
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    }
 
-  function resetCount() {
-    setCount(0);
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    }
+
+  function reset() {
+    setAll([]);
+    setLeft(0);
+    setRight(0);
   }
 
   return (
     <div>
-      <Header course={course.name}></Header>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-      <p>{count}</p>
-      <Button label={"+"} onClick={increment}/>
-      <Button label={"-"} onClick={() => setCount(i => i - 1)}/>
-      <Button label={"Reset"} onClick={resetCount}/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History clickHistory={allClicks}/>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
 
-
-function Button({label, onClick}) {
-  return <button onClick={onClick}>{label}</button>
-}
-
-function Header({course}) {
-  return <h1>{course}</h1>
-}
-
-function Content({parts}) {
-  return (
-    <>
-      {parts.map((c, idx) => <p key={idx}>{c.name} - {c.exercises}</p>)}
-    </>
-  )
-}
-
-function Total({parts}) {
-  const total = parts.reduce((a, c) => c.exercises + a, 0)
-
-  return <p>Total exercises - {total}</p>
+function History({ clickHistory }) {
+  let result;
+  if (clickHistory.length > 0) {
+    result = (
+      <>
+        <p>History:{clickHistory.join("")}</p>
+        <p>Total: {clickHistory.length}</p>
+      </>
+    )
+  } else {
+    result = <p>Click a button to get started!</p>
+  }
+  return <div>{result}</div>
 }
 
 export default App
