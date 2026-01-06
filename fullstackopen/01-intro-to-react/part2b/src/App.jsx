d246f9e34f1ab0dff0e4fc124b161a83067362ed
SValenziano <senorvalenz@gmail.com>
2026-01-06 16:52:11 -0500

    2.6 complete

--- fullstackopen/01-intro-to-react/part2b/src/App.jsx
+++ fullstackopen/01-intro-to-react/part2b/src/App.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react'
 
+const defaultPeople = [{ id: 0, name: 'Arto Hellas' }];
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState(defaultPeople);
+  const [newName, setNewName] = useState('');
+  const maxId = persons.reduce((accum, person) => Math.max(accum, person.id), 0)
+
+  function newContact(ev) {
+    ev.preventDefault();
+    setPersons([
+      ...persons,
+      {id: maxId + 1, name: newName}
+    ])
+    setNewName('');
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={newContact}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" disabled={!newName}>+</button>
         </div>
       </form>
+      <Phonebook persons={persons}/>
+    </div>
+  )
+}
+
+function Phonebook({ persons }) {
+  return (
+    <div>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(p => <li key={p.id}>{p.name}</li>)}
+      </ul>
     </div>
   )
 }
