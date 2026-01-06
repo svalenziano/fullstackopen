ddb37a0f07ed4ac376af1eb22a036742b47ea73b
SValenziano <senorvalenz@gmail.com>
2026-01-06 17:25:16 -0500

    2.9 done

--- fullstackopen/01-intro-to-react/part2b/src/App.jsx
+++ fullstackopen/01-intro-to-react/part2b/src/App.jsx
@@ -10,13 +10,18 @@ const App = () => {
   const [persons, setPersons] = useState(defaultPeople);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const filteredPersons = filter 
+    ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase())) 
+    : persons;
   const maxId = persons.reduce((accum, person) => Math.max(accum, person.id), 0);
   const names = persons.map(p => p.name);
 
   function newContact(ev) {
     ev.preventDefault();
-    console.log(newName);
-    console.log(names);
+    // console.log(newName);
+    // console.log(names);
     if (names.includes(newName)) {
       alert(`"${newName}" is already in the phonebook. Try a different name.`)
       return;
@@ -37,7 +42,33 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={newContact}>
+      <Filter filter={filter} setFilter={setFilter}/>
+      <AddNew onSubmit={newContact} newName={newName} newPhone={newPhone} setNewName={setNewName} setNewPhone={setNewPhone}/>
+      <Phonebook persons={filteredPersons}/>
+    </div>
+  )
+}
+
+function Filter({ filter, setFilter }) {
+  return (
+    <div>
+      <h3>Filter</h3>
+      type and smile: <input type="text" value={filter} onChange={e => setFilter(e.target.value)}/>
+    </div>
+  )
+}
+
+function AddNew({
+  onSubmit,
+  newName,
+  newPhone,
+  setNewName,
+  setNewPhone,
+}) {
+  return (
+    <div>
+      <h3>Add new</h3>
+      <form onSubmit={onSubmit}>
         <div>
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
@@ -48,18 +79,19 @@ const App = () => {
           <button type="submit" disabled={!newName}>+</button>
         </div>
       </form>
-      <Phonebook persons={persons}/>
     </div>
   )
 }
 
 function Phonebook({ persons }) {
+  const toDisplay = persons.length > 0
+    ? <ul>{persons.map(p => <li key={p.id}>{p.name} {p.phone ? p.phone : ''}</li>)}</ul>
+    : <p>Nothing to see here</p>
+    
   return (
     <div>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(p => <li key={p.id}>{p.name} {p.phone ? p.phone : ''}</li>)}
-      </ul>
+      <h3>Numbers</h3>
+      {toDisplay}
     </div>
   )
 }
