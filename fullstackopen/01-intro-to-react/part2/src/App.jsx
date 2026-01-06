ef6d9375cdcd288d291cd811ca5495089009e413
SValenziano <senorvalenz@gmail.com>
2026-01-06 15:16:36 -0500

    2.5 done

--- fullstackopen/01-intro-to-react/part2/src/App.jsx
+++ fullstackopen/01-intro-to-react/part2/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./Course"
+
 const App = () => {
   const courses = [
     {
@@ -44,56 +46,14 @@ const App = () => {
     }
   ]
 
-
   return (
     <>
     <h1>Curriculum</h1>
       {courses.map((c) => {
-        return <Course course={c} />
+        return <Course key={c.id} course={c} />
       })}
     </>
   )
 }
-
-function Course({ course }) {
-  console.log(course.parts)
-  return (
-    <div>
-      <Header text={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-function Header({ text }) {
-  return <h2>{text}</h2>
-}
-
-function Content({ parts }) {
-  return (
-    <div>
-      {parts.map(p => <Part key={p.id} name={p.name} exercises={p.exercises} />)}
-    </div>
-  )
-}
-
-function Part({ name, exercises }) {
-  return <p>{name} {exercises}</p>
-}
-
-function Total({ parts }) {
-  return <p>Total exercises: {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-}
-
-/*
-App
-  Course
-    Header
-    Content
-      Part
-      Part
-      ...
-*/
 
 export default App
