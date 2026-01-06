46bd593e8b716f73bf0b75133ed48dff7dc3fc37
SValenziano <senorvalenz@gmail.com>
2026-01-06 16:36:51 -0500

    'phonebook' initial setup

--- fullstackopen/01-intro-to-react/part2b/src/App.jsx
+++ fullstackopen/01-intro-to-react/part2b/src/App.jsx
@@ -1,48 +1,26 @@
 import { useState } from 'react'
-import Note from './components/Note'
 
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newText, setNewText] = useState('');
-  const maxId = notes.reduce((accum, note) => Math.max(accum, note.id), 0)
-  const disable = newText.length === 0 ? "disabled" : "";
-  console.log(disable)
-
-  const updateForm = e => setNewText(e.target.value);
-
-  function submit(ev) {
-    ev.preventDefault();
-    /*
-    Create new note using maxId
-    Update notes
-    */
-    setNotes([
-      ...notes,
-      {
-        id: maxId + 1,
-        content: newText,
-        important: Math.random() < 0.5,
-      }
-    ]);
-    setNewText('')
-  }
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      <h1>Notes</h1>
-      <form onSubmit={submit}>
-        <label htmlFor="new-note">New note: </label>
-        <input name="new-note" type="text" value={newText} onChange={updateForm}/>
-        <input type="submit" disabled={disable}/>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
 
-export default App 
+export default App
