3c6787f453b8fa58db20d3d0de22f98a9983e254
SValenziano <senorvalenz@gmail.com>
2026-01-09 13:46:21 -0500

    3.2 complete

---
+++ fullstackopen/03-server-express-node/phonebook/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const entries = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${entries.length} people</p>
+    <p>${Date()}</p>
+  `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(entries);
+})
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
