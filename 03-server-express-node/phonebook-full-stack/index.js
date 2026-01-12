fab9d61a06f31758400cf665e776c467d10c464a
SValenziano <senorvalenz@gmail.com>
2026-01-12 15:39:07 -0500

    Setup 'static' routes

--- 03-server-express-node/phonebook-full-stack/index.js
+++ 03-server-express-node/phonebook-full-stack/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => req.body ? JSON.stringify(req.body) : "{}")
 
 app.use(morgan('tiny', {
   skip: (req, res) => req.method === "POST"
 }));
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
   skip: (req, res) => req.method !== "POST"
 }));
