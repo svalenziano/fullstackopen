9873645284c0b18228605899fe18eeaabcacb80f
SValenziano <senorvalenz@gmail.com>
2026-01-12 15:33:04 -0500

    Deploy

--- 03-server-express-node/phonebook-full-stack/build-ui.sh
+++ 03-server-express-node/phonebook-full-stack/build-ui.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 FRONTEND=../../02-com-with-server/phonebook/
 echo "Deleting existing build files from backend"
 \rm -rf ./dist
 echo "Building frontend located at: $FRONTEND"
-cd $FRONTEND || exit 1
+pushd $FRONTEND || exit 1
 npm run build
-cd - || exit 1
+popd || exit 1
 cp -r "$FRONTEND/dist" ./dist
 echo "Build complete"
