9d6e49504ecc023315899ee649560d04e5ca271e
SValenziano <senorvalenz@gmail.com>
2026-01-12 15:27:02 -0500

    replace pushd and popd with cd and cd -

--- 03-server-express-node/phonebook-full-stack/build-ui.sh
+++ 03-server-express-node/phonebook-full-stack/build-ui.sh
@@ -2,8 +2,8 @@ FRONTEND=../../02-com-with-server/phonebook/
 echo "Deleting existing build files from backend"
 \rm -rf ./dist
 echo "Building frontend located at: $FRONTEND"
-pushd $FRONTEND
+cd $FRONTEND || exit 1
 npm run build
-popd
+cd - || exit 1
 cp -r "$FRONTEND/dist" ./dist
 echo "Build complete"
