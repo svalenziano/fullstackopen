729b8ececf50fe55c9593e173b208ad9388b66d4
SValenziano <senorvalenz@gmail.com>
2026-01-13 16:03:06 -0500

    Extract some mongo func to note.js

--- 03-server-express-node/notes/index.js
+++ 03-server-express-node/notes/index.js
@@ -1,41 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+const {noteSchem, Note} = require('./models/note.js')
 
 
 const app = express()
 const PORT = process.env.PORT || 3001
-const MONGO_URI = encodeURI(process.env.MONGO_URI);
-
-mongoose.set('strictQuery', false);
-mongoose.connect(MONGO_URI, { family: 4 })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 
 
-// create Schema
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
 
-// create model from Schema
-const Note = mongoose.model('Note', noteSchema);
 
-// map id to string
-noteSchema.set('toJSON', {
-  transform: (doc, returnedObj) => {
-    returnedObj.id = returnedObj._id.toString();
-    delete returnedObj._id;
-    delete returnedObj.__v;
-  }
-})
+
 
 // fetch notes
 let notes = [];
---
+++ 03-server-express-node/notes/models/note.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+const MONGO_URI = encodeURI(process.env.MONGO_URI);
+
+mongoose.connect(MONGO_URI, { family: 4 })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+// create Schema
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// create model from Schema
+const Note = mongoose.model('Note', noteSchema);
+
+// map id to string
+noteSchema.set('toJSON', {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  }
+})
+
+module.exports = {
+  noteSchema,
+  Note,
+}
