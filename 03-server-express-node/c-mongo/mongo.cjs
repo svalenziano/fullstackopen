ecdae4bd8d5e5c1bca624eb29815a14ed4fceb2b
SValenziano <senorvalenz@gmail.com>
2026-01-13 11:55:39 -0500

    👍Mongo write test

---
+++ 03-server-express-node/c-mongo/mongo.cjs
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const PASSWORD = encodeURI(process.argv[2])
+
+const url = `mongodb+srv://stvn:${PASSWORD}@cluster0.e8ozpjc.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
