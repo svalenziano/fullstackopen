24d5d8ee4c84e1c267bc8d603d306339c55d6054
SValenziano <senorvalenz@gmail.com>
2026-01-13 12:30:10 -0500

    broken, not sure why

--- 03-server-express-node/c-mongo/mongo.cjs
+++ 03-server-express-node/c-mongo/mongo.cjs
@@ -7,25 +7,57 @@ if (process.argv.length < 3) {
 
 const PASSWORD = encodeURI(process.argv[2])
 
-const url = `mongodb+srv://stvn:${PASSWORD}@cluster0.e8ozpjc.mongodb.net/?appName=Cluster0`
+const url = `mongodb+srv://stvn:${PASSWORD}@cluster0.e8ozpjc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
-mongoose.connect(url, { family: 4 })
+mongoose.connect(url, { family: 4 }) // {family: 4} => Atlas only supports IPv4
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
+/*
+Create Model from Schema
+
+👍Mongoose automatically creates a lowercased plural collection from your model name.  
+
+EG, 'Note' model name => 'notes' collection name
+*/
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+function createAndSaveDocument() {
+  // create document from Model
+  const note = new Note({
+    content: 'HTML is easy',
+    important: true,
+  })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+  // save document to the database
+  note.save().then(result => {
+    console.log('note saved!')
+  })
+
+}
+
+/*
+Your DB now contains a 'notes' collection (whether or not it had one before)
+*/
+
+function fetchAllAndPrint() {
+  Note.find({})
+    .then(result => {
+      result.forEach(note => {
+        console.log(note);
+      })
+    })
+}
+
+
+/*
+RUN TESTS
+*/
+createAndSaveDocument();
+// fetchAllAndPrint();
+mongoose.connection.close();
