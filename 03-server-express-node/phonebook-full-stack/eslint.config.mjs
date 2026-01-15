7ef4ee1a986a35ebfc522efb5b8350bb2d2701e6
SValenziano <senorvalenz@gmail.com>
2026-01-15 10:14:53 -0500

    eslint config done

--- 03-server-express-node/phonebook-full-stack/eslint.config.mjs
+++ 03-server-express-node/phonebook-full-stack/eslint.config.mjs
@@ -1,14 +1,32 @@
 import globals from "globals";
 import js from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin';
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ['**/*.js', '**/*.cjs'],
     languageOptions: {
       sourceType: 'commonjs',
       globals: { ...globals.node },
       ecmaVersion: 'latest',
     },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      'no-console': 'off',
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    }
+  },
+  {
+    ignores: ['dist/**'],
   },
 ]
