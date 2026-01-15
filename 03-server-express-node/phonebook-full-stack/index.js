26b3c37aeb0a1bbcb2f64d530a6032535232280c
SValenziano <senorvalenz@gmail.com>
2026-01-15 10:33:03 -0500

    done: Phonebook lint

--- 03-server-express-node/phonebook-full-stack/index.js
+++ 03-server-express-node/phonebook-full-stack/index.js
@@ -1,86 +1,58 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 const PORT = process.env.PORT || 3001
-const { Entry } = require("./models/entry.js")
+const { Entry } = require('./models/entry.js')
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.static('dist'))
 
 
 //////////////////////////////////////////////////////////////////////////////
 // LOGGING
 
-morgan.token('body', (req, res) => req.body ? JSON.stringify(req.body) : "{}")
+morgan.token('body', (req) => req.body ? JSON.stringify(req.body) : '{}')
 
 app.use(morgan('tiny', {
-  skip: (req, res) => req.method === "POST" || req.method === 'PATCH'
-}));
+  skip: (req) => req.method === 'POST' || req.method === 'PATCH'
+}))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
-  skip: (req, res) => req.method !== "POST" && req.method !== "PATCH"
-}));
+  skip: (req) => req.method !== 'POST' && req.method !== 'PATCH'
+}))
 
 
 //////////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
 
-function errorHandler(error, request, response, next) {
-  if (error.name === "CastError") {
-    return response.status(400).json({ error: "Malformed request" });
-  } else if (error.name === "ValidationError") {
+function errorHandler(error, request, response) {
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'Malformed request' })
+  } else if (error.name === 'ValidationError') {
     console.log('TRIPPED VALIDATION ERROR')
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  response.status(500).json({ error: "Unhandled server error ☹️" })
+  response.status(500).json({ error: 'Unhandled server error ☹️' })
   // next(error);
 }
 
 function makeErrorObjectIdNotFound(id) {
-  return {error: `No person with id ${id} found.`};
+  return { error: `No person with id ${id} found.` }
 }
 
-/**
- * Remember that value of `important` is a string: `true` or `false`
- * @param {*} entries 
- * @param {*} id id of the entry to toggle `important`
- */
-function updateEntry(entries, id, data) {
-  return entries.map(entry => {
-      if (entry.id === id) {
-        const modified = {
-          ...entry,
-          ...data
-        };
-
-        return modified;
-      } else {
-        return entry;
-      }
-    });
-}
 
 function handleUnknownEndpoint(req, res) {
-  return res.status(404).send({ error: 'unknown endpoint' });
-}
-
-
-function findEntryById(obj, id) {
-  return obj?.find(entry => entry.id === id);
-}
-
-function nameExists(obj, name) {
-  return !!obj.find(i => i.name === name)
+  return res.status(404).send({ error: 'unknown endpoint' })
 }
 
 function getAll(req, res) {
   Entry.find({})
     .then(result => {
-      res.json(result);
-      return result;
-    }).catch(er => {
+      res.json(result)
+      return result
+    }).catch(() => {
       res.status(404).end()
     })
 }
@@ -91,50 +63,50 @@ function getAll(req, res) {
 
 app.get('/info', (req, res) => {
 
-  let count = null;
+  let count = null
 
   Entry.countDocuments({})
     .then(c => {
-      count = c;
+      count = c
     }).finally(() => {
       res.send(`
-        <p>Phonebook has info for ${count ?? "UNKNOWN"} people</p>
+        <p>Phonebook has info for ${count ?? 'UNKNOWN'} people</p>
         <p>${Date()}</p>
-      `);
+      `)
     })
 
 })
 
 app.get('/api/persons', (req, res) => {
-  getAll(req, res);
+  getAll(req, res)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
 
   Entry.findById(id)
     .then(result => {
-      res.json(result);
-    }).catch(er => {
-      res.status(404).json(makeErrorObjectIdNotFound(id));
+      res.json(result)
+    }).catch(() => {
+      res.status(404).json(makeErrorObjectIdNotFound(id))
     })
-}) 
+})
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
 
   Entry.findByIdAndDelete(id)
     .then(result => {
-      console.log(result);
-      res.json(result);
-    }).catch(er => {
-      res.status(404).json(makeErrorObjectIdNotFound(id));
+      console.log(result)
+      res.json(result)
+    }).catch(() => {
+      res.status(404).json(makeErrorObjectIdNotFound(id))
     })
-});
+})
 
 app.post('/api/persons', (req, res, next) => {
 
-  const { name, number } = req.body;
+  const { name, number } = req.body
 
   const entry = new Entry({
     name,
@@ -143,26 +115,26 @@ app.post('/api/persons', (req, res, next) => {
 
   entry.save()
     .then(result => {
-      res.json(result);
+      res.json(result)
     }).catch(er => next(er))
 })
 
 app.patch('/api/persons/:id', (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
     console.error('Unexpected body')
-    console.error(req.body);
+    console.error(req.body)
   }
-  
-  const id = req.params.id;
 
-  console.log("UPDATE ROUTE", req.body)
+  const id = req.params.id
 
-  Entry.findByIdAndUpdate(id, req.body, {new: true})
+  console.log('UPDATE ROUTE', req.body)
+
+  Entry.findByIdAndUpdate(id, req.body, { new: true })
     .then(result => {
-      res.json(result);
+      res.json(result)
     }).catch(er => {
-      console.error('SOMETHING WENT WRONG');
-      console.error(er);
+      console.error('SOMETHING WENT WRONG')
+      console.error(er)
       res.status(500).json(makeErrorObjectIdNotFound(id))
     })
 })
@@ -171,9 +143,9 @@ app.patch('/api/persons/:id', (req, res) => {
 //////////////////////////////////////////////////////////////////////////////
 // More Middleware
 
-app.use(handleUnknownEndpoint);
-app.use(errorHandler);
+app.use(handleUnknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-});
+})
--- 03-server-express-node/phonebook-full-stack/models/entry.js
+++ 03-server-express-node/phonebook-full-stack/models/entry.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
-const MONGO_URI = encodeURI(process.env.MONGO_URI);
-console.log("\nMONGO_URI", MONGO_URI, "\n");
+mongoose.set('strictQuery', false)
+const MONGO_URI = encodeURI(process.env.MONGO_URI)
+console.log('\nMONGO_URI', MONGO_URI, '\n')
 
 mongoose.connect(MONGO_URI, { family: 4 })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -21,7 +21,7 @@ const entrySchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: [true, "Phone number is required!"],
+    required: [true, 'Phone number is required!'],
     validate: {
       validator: (value) => /\d{3}-\d{4}/.test(value),
       message: (props) => `${props.value} is not a valid phone number`,
@@ -31,14 +31,14 @@ const entrySchema = new mongoose.Schema({
 })
 
 // create model from Schema
-const Entry = mongoose.model('Entry', entrySchema);
+const Entry = mongoose.model('Entry', entrySchema)
 
 // map id to string and remove excess properties
 entrySchema.set('toJSON', {
   transform: (doc, returnedObj) => {
-    returnedObj.id = returnedObj._id.toString();
-    delete returnedObj._id;
-    delete returnedObj.__v;
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
   }
 })
 
